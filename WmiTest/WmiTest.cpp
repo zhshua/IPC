8545a8e7c14df627a814856ff3cbbca0cdff2e72
zhang shuai <869528598@qq.com>
2020-06-10 23:55:20 +0800

    增加双进程

--- WmiTest/WmiTest.cpp
+++ WmiTest/WmiTest.cpp
@@ -57,9 +57,9 @@ int main()
 	cout << "process over" << endl;
 	wmi.ReleaseWmi();
 	char TwinCatPath[] = "C:\\TwinCAT";
-	char fileName1[] = "C:\\data\\industrial_system_files.txt";
+	char fileName1[] = "C:\\data\\txtdata\\industrial_system_files.txt";
 	//writeTime(fileName1);
-	char fileName2[] = "C:\\data\\industrial_programming_files.txt";
+	char fileName2[] = "C:\\data\\txtdata\\industrial_programming_files.txt";
 	//writeTime(fileName2);
 	getTwinCatInfo(TwinCatPath);
 	cout << "TwinCatInfo over" << endl;
@@ -67,6 +67,7 @@ int main()
 	cout << "port over" << endl;
 	getRegitHash();
 	cout << "regedit over" << endl;
+	return 0;
 }
 
 void writeTime(const char fileName[]) {
@@ -176,7 +177,7 @@ void getSysInfo(CWmiInfo wmi) {
 	char ip[20];
 	getIP(ip);
 
-	char fileName[] = "C:\\data\\sysinfo.txt";
+	char fileName[] = "C:\\data\\txtdata\\sysinfo.txt";
 	writeTime(fileName);
 	ofstream file(fileName, ios::out | ios::app);
 	if (file.is_open())
@@ -289,7 +290,7 @@ void getDisk(CWmiInfo wmi) {
 	char freeSize[10][500];
 	Split(freeSize, strRetValue, "\n");
 
-	char fileName[] = "C:\\data\\disk.txt";
+	char fileName[] = "C:\\data\\txtdata\\disk.txt";
 	ofstream file(fileName);
 	if (file.is_open()) {
 		for (int i = 0; i < num; i++) {
@@ -317,7 +318,7 @@ void getDisk(CWmiInfo wmi) {
 	wmi.GetSingleItemInfo(_T("Win32_DiskDrive"), _T("SerialNumber"), strRetValue);
 	char SerialNumber[10][500];
 	num = Split(SerialNumber, strRetValue, "\n");
-	char fileName1[] = "C:\\data\\mobile_device.txt";
+	char fileName1[] = "C:\\data\\txtdata\\mobile_device.txt";
 	ofstream file1(fileName1);
 	if (file1.is_open()) {
 		int j = 1;
@@ -356,7 +357,7 @@ void getMemory(CWmiInfo wmi) {
 	allSize = ((float)((int)((allSize + 0.005) * 100))) / 100;
 	memoryPre = ((float)((int)((memoryPre + 0.005) * 100))) / 100;
 
-	char fileName[] = "C:\\data\\memory.txt";
+	char fileName[] = "C:\\data\\txtdata\\memory.txt";
 	writeTime(fileName);
 	ofstream file(fileName);
 	if (file.is_open())
@@ -406,7 +407,7 @@ void getTwinCatInfo(const char* dir) {
 				char sha1[80];
 				getSha1(fileName, sha1);
 
-				char writeFileName[] = "C:\\data\\industrial_system_files.txt";
+				char writeFileName[] = "C:\\data\\txtdata\\industrial_system_files.txt";
 				ofstream writeFile(writeFileName, ios::out | ios::app);
 				if (writeFile.is_open())
 				{
@@ -429,7 +430,7 @@ void getTwinCatInfo(const char* dir) {
 				char sha1[80];
 				getSha1(fileName, sha1);
 
-				char writeFileName[] = "C:\\data\\industrial_programming_files.txt";
+				char writeFileName[] = "C:\\data\\txtdata\\industrial_programming_files.txt";
 				ofstream writeFile(writeFileName, ios::out | ios::app);
 				if (writeFile.is_open())
 				{
@@ -454,7 +455,7 @@ void getPatchInfo(CWmiInfo wmi) {
 	char patchID[200][500];
 	char patchFrom[200][500];
 	char installTime[200][500];
-	char fileName[] = "C:\\data\\patch.txt";
+	char fileName[] = "C:\\data\\txtdata\\patch.txt";
 
 	wmi.GetSingleItemInfo(_T("Win32_QuickFixEngineering"), _T("HotFixID"), strRetValue);
 	int len = Split(patchID, strRetValue, "\n");
@@ -487,8 +488,8 @@ void getService(CWmiInfo wmi) {
 	CString strRetValue;
 	char serviceName[300][500];
 	char serviceAddress[300][500];
-	char fileName1[] = "C:\\data\\system_services.txt";
-	char fileName2[] = "C:\\data\\notsystem_services.txt";
+	char fileName1[] = "C:\\data\\txtdata\\system_services.txt";
+	char fileName2[] = "C:\\data\\txtdata\\notsystem_services.txt";
 
 	char sysName1[] = "C:\\Windows\\system32";
 	char sysName2[] = "C:\\Windows\\System32";
@@ -535,7 +536,7 @@ void getService(CWmiInfo wmi) {
 void getUserAccount(CWmiInfo wmi) {
 	CString strRetValue;
 	char name[100][500];
-	char fileName[] = "C:\\data\\login_user.txt";
+	char fileName[] = "C:\\data\\txtdata\\login_user.txt";
 
 	wmi.GetSingleItemInfo(_T("Win32_UserAccount WHERE Disabled='False'"), _T("Name"), strRetValue);
 	int len = Split(name, strRetValue, "\n");
@@ -600,7 +601,7 @@ void getExeInfo(CWmiInfo wmi) {
 	char publish[500][500];
 	int len3 = Split(publish, strRetValue, "\n");
 
-	char fileName[] = "C:\\data\\application.txt";
+	char fileName[] = "C:\\data\\txtdata\\application.txt";
 	ofstream file(fileName);
 	for (int i = 0; i < len1; i++) {
 		if (file.is_open())
@@ -628,7 +629,7 @@ void getProcess(CWmiInfo wmi) {
 	char path[500][500];
 	int len2 = Split(path, strRetValue, "\n");
 
-	char fileName[] = "C:\\data\\process.txt";
+	char fileName[] = "C:\\data\\txtdata\\process.txt";
 	ofstream file(fileName);
 	for (int i = 3; i < len1; i++) {
 		if (file.is_open())
@@ -721,7 +722,7 @@ void getPort() {
 			portArray[j] = port;
 		}
 	}
-	char fileName[] = "C:\\data\\network_port.txt";
+	char fileName[] = "C:\\data\\txtdata\\network_port.txt";
 	ofstream file(fileName);
 	i = 0;
 	while (portArray[i] != 0) {
@@ -780,7 +781,7 @@ void getRegitHash() {
 	getSha1("C:\\HKCC.reg", sha1[2]);
 	getSha1("C:\\HKLM.reg", sha1[3]);
 	getSha1("C:\\HKU.reg", sha1[4]);
-	char fileName[] = "C:\\data\\regedit.txt";
+	char fileName[] = "C:\\data\\txtdata\\regedit.txt";
 	ofstream file(fileName);
 	if (file.is_open()){
 		file << "name:HKCR";
